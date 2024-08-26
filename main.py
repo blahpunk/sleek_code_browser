e949dedfef33bfb481d766e9976c12b2b2f15fb4
blah-punk <eric.zeigenbein@gmail.com>
2024-08-26 00:10:03 -0500

    Added application icon and updated main.py to use icon.ico

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from ui_logic import UiLogic
 from ui_setup import UiSetup
 from styles import apply_styles
+from PyQt5.QtGui import QIcon
+
 
 class FileBrowserApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui_setup = UiSetup(self)
-        self.ui_logic = UiLogic(self.ui_setup)  # Only pass the ui_setup now
+        self.ui_logic = UiLogic(self.ui_setup)
         apply_styles(self)
+        self.setWindowIcon(QIcon('icon.ico'))  # Set the window icon
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle('Sleek File Browser')
         self.setGeometry(100, 100, 800, 600)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
